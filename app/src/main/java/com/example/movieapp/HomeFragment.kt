25d88bde8a52fd166bd06f57792ff36d179473db
ts moosavi <tt.musavi@gmail.com>
2022-03-10 01:11:06 +0330

    menu showed in home fragment

--- app/src/main/java/com/example/movieapp/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/HomeFragment.kt
@@ -1,10 +1,8 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.movieapp.databinding.FragmentHomeBinding
 
 
@@ -12,6 +10,7 @@ class HomeFragment : Fragment() {
         lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+         setHasOptionsMenu(true)
 
     }
 
@@ -24,4 +23,39 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.profile ->{
+                goToProfileFragmint()
+                true
+            }
+            R.id.favorit ->{
+                 goToFavoriteFragmint()
+                 true
+            }
+            R.id.comingSoon->{
+                goToComingSoonFragmint()
+                true
+            }
+            else ->return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun goToComingSoonFragmint() {
+        TODO("Not yet implemented")
+    }
+
+    private fun goToFavoriteFragmint() {
+        TODO("Not yet implemented")
+    }
+
+    private fun goToProfileFragmint() {
+        TODO("Not yet implemented")
+    }
 }
