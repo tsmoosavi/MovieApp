b05fadc365b3f1de1c7adcb4e1c725cbfcccd2c0
ts moosavi <tt.musavi@gmail.com>
2022-03-11 02:03:27 +0330

    a function added to change star buttons'color

--- app/src/main/java/com/example/movieapp/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/HomeFragment.kt
@@ -2,18 +2,21 @@ package com.example.movieapp
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.movieapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-        lateinit var binding: FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setHasOptionsMenu(true)
+        setHasOptionsMenu(true)
 
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +25,30 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
         return binding.root
+
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.star1.setBackgroundResource(R.color.black)
+        changeStarColor()
+//        binding.star1.setOnClickListener{
+//            binding.star1.setBackgroundResource(R.color.black)
+//        }
+
+    }
+    fun changeStarColor(){
+
+        var buttonList = arrayListOf(binding.star1,binding.star2,binding.star3,
+            binding.star4,binding.star5,binding.star6,
+            binding.star7,binding.star8,binding.star9,
+            binding.star10,binding.star11,binding.star12)   //wher should I put this button list????
+        for (button in buttonList){
+            button.setOnClickListener{
+                button.setBackgroundResource(R.color.yellow)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
